82555228b387b838bbf990c075ee69a1c9564432
ImRyzon <imtheryzon@gmail.com>
2022-04-17 19:57:38 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static FastIO r = new FastIO();
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static void main(String[] args) throws Exception {
+        int n = r.readInt(), max = r.readInt(), w[] = new int[n];
+        long v[] = new long[n], dp[] = new long[max+1];
+        for (int i = 0; i < n; i++) {w[i] = r.readInt(); v[i] = r.readLong();}
+        for (int i = 1; i <= n; i++) {
+            for (int j = max; j >= 0; j--) {if (w[i-1] <= j) dp[j] = Math.max(dp[j], dp[(int)(j-w[i-1])] + v[i-1]);}
+        }
+        pr.println(dp[max]); pr.close();
+    }
+
+    static class FastIO extends PrintWriter {
+        private InputStream stream;
+        private byte[] buf = new byte[1 << 16];
+        private int curChar, numChars;
+        public FastIO() {this(System.in, System.out);}
+        public FastIO(InputStream i, OutputStream o) {super(o);stream = i;}
+        public FastIO(String i, String o) throws IOException {
+            super(new FileWriter(o)); stream = new FileInputStream(i);
+        }
+        private int readByte() {
+            if (numChars == -1) {throw new InputMismatchException();}
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {numChars = stream.read(buf);
+                }catch(Exception e){throw new InputMismatchException();}
+                if (numChars == -1) {return -1;}
+            }
+            return buf[curChar++];
+        }
+        public String next() {
+            int c; do {c = readByte();} while (c <= ' ');
+            StringBuilder res = new StringBuilder();
+            do {res.appendCodePoint(c);c = readByte();} while (c > ' ');
+            return res.toString();
+        }
+        public String readLine() {
+            int c; do {c = readByte();} while (isEndLine(c));
+            StringBuilder res = new StringBuilder();
+            do {res.appendCodePoint(c);c = readByte();} while (c >= ' ');
+            return res.toString();
+        }
+        public int readInt() {
+            int c, sgn = 1, res = 0;
+            do {c = readByte();} while (c <= ' ');
+            if (c == '-') {sgn = -1;c = readByte();}
+            do {
+                if (c < '0' || c > '9') {throw new InputMismatchException();}
+                res = 10 * res + c - '0';c = readByte();
+            } while (c > ' ');
+            return res * sgn;
+        }
+        public double readDouble() {return Double.parseDouble(next());}
+        public long readLong() {return Long.parseLong(next());}
+        boolean isEndLine(int c) {return c == '\n' || c == '\r' || c == -1;}
+    }
+}

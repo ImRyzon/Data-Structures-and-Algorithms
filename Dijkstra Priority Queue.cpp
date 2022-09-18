5290f345ddaba72d806acf6299e7ff169e633844
ImRyzon <imtheryzon@gmail.com>
2022-09-17 20:57:53 -0400

    Create Dijkstra Priority Queue.cpp

---
+++ Dijkstra Priority Queue.cpp
@@ -0,0 +1,23 @@
+void dijkstra(int start, int dis[]) {
+    for (int i = 0; i < MM; i++) {
+        vis[i] = false;
+        dis[i] = 0x3F3F3F3F;
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dis[start] = 0;
+    pq.push({dis[start], start});
+    while (!pq.empty()) {
+        pair<int, int> x = pq.top();
+        pq.pop();
+        if (!vis[x.second]) {
+            vis[x.second] = true;
+            for (auto &e : adj[x.second]) {
+                int v = e.first, w = e.second;
+                if (!vis[v] && dis[x.second] + w < dis[v]) {
+                    dis[v] = dis[x.second] + w;
+                    pq.push({dis[v], v});
+                }
+            }
+        }
+    }
+}

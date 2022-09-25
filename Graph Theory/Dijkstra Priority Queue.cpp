377ec2884d5b1e55cdb7ed667aefd49ae5edd1f4
Ryzon <imtheryzon@gmail.com>
2022-09-25 18:23:22 -0400

    Update Dijkstra Priority Queue.cpp

--- Graph Theory/Dijkstra Priority Queue.cpp
+++ Graph Theory/Dijkstra Priority Queue.cpp
@@ -28,10 +28,8 @@ int dis[MM], N, M;
 bool vis[MM];
 
 void dijkstra(int start) {
-    for (int i = 0; i < MM; i++) {
-        vis[i] = false;
-        dis[i] = 0x3F3F3F3F;
-    }
+    fill(vis, vis + N + 1, false);
+    fill(dis, dis + N + 1, INT_MAX);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     dis[start] = 0;
     pq.push({dis[start], start});
